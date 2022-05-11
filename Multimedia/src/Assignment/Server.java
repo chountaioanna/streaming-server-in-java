3100d4af52bc20e07646f524826e94e9fb802ab7
chountaioanna <93378781+chountaioanna@users.noreply.github.com>
2022-05-11 15:11:29 +0300

    Server generate alal files they need for temp.size = 1
    Needs attention:
    The generated files should not have match with the initial list of videos
    Specifically, with initial list
    Earth 360p.mkv
    Earth 720p.mp4
    the second file refuses to shake its ass

--- Multimedia/src/Assignment/Server.java
+++ Multimedia/src/Assignment/Server.java
@@ -1,42 +1,30 @@
 package Assignment;
 
-import java.net.ServerSocket;
-
 import java.io.File;
 import java.util.ArrayList;
 
 public class Server 
 {
-	private static ServerSocket server;
-	private static int port = 5000;
+	private String[] format = {"avi","mp4","mkv"};
+	private String[] resolution = {"1080p","720p","480p","360p","240p"};
 	
-	private ArrayList<String> format;
-	private ArrayList<String> resolution;
+	private static ArrayList<Video> availableVideos = new ArrayList<Video>();
+	private static ArrayList<Video> missingVideos = new ArrayList<Video>();
 	
-	public Server ()
-	{
-		format = new ArrayList<String>();
-		format.add("avi");
-	    format.add("mp4");
-	    format.add("mkv");
-	    
-	    resolution = new ArrayList<String>();
-	    resolution.add("240p");
-	    resolution.add("360p");
-	    resolution.add("480p");
-	    resolution.add("720p");
-	    resolution.add("1080p");
-	}
-	
+
 	public static void main(String args[]) 
 	{
 		Server server = new Server();
 		String DirectoryPath = System.getProperty("user.dir") + "/videos/";
-		server.checkVideos(DirectoryPath);
-		
+		server.analyzeVideos(DirectoryPath); //turn videos in objects
+		server.getMissingVideos(); // get list of videos that ffmpeg is needed
+		for (Video video: missingVideos)
+		{
+			video.showVideoDetails();
+		}
 	}
 	
-	public void checkVideos (String DirectoryPath)
+	public void analyzeVideos (String DirectoryPath)
 	{
 		File dir = new File(DirectoryPath);
 		File[] directoryListing = dir.listFiles();
@@ -44,12 +32,69 @@ public class Server
 		{
 		    for (File child : directoryListing) 
 		    {
-		      System.out.println(child.getName());
+		    	String[] child_details;
+		    	child_details = child.getName().split("-|\\.");
+		    	Video video = new Video(child_details[0],child_details[1],child_details[2]);
+		    	availableVideos.add(video);	
 		    }
-		} 
-		else 
+		}
+	}
+	
+	public void getMissingVideos ()
+	{
+		ArrayList<Video> temp = new ArrayList<Video>();
+		for(Video video: availableVideos)
 		{
-		    //TODO: Handle the case where dir is not really a directory or there are not videos
+			// TODO: na ftiakso omorfo kvdika poy nmhn kanei ta idia pragmata dyo fores
+			temp = video.searchByName(availableVideos); // lista me objects poy aforoyn ton idio titlo
+			if( temp.size() == 1 ) 
+			{
+				String max_resolution_value = video.getResolution();
+				int max_resolution_loc=0;
+				for (int i=0; i<resolution.length; i++)
+				{
+					if(resolution[i].equals(max_resolution_value))
+					{
+						max_resolution_loc = i;
+					}
+				}
+				missingVideos.addAll(video.getMissingVideos(format,resolution,max_resolution_loc));
+			}
+			else
+			{
+				// TODO: bres thn megalyterh resolution apo thn lista me ta temp kai krata aythn
+				String max_resolution_value = temp.get(0).getResolution();
+				int max_resolution_loc=0;
+				for(int i=1; i<temp.size(); i++)
+				{
+					if(temp.get(i).hasBetterResolution(max_resolution_value))
+					{
+						max_resolution_value = temp.get(i).getResolution();
+						max_resolution_loc = i;
+					}
+				} 
+				
+				// TODO: thelo na pigainei ola ta objects toy temp plhn toy video me thn max resolution sthn lista already_have
+				ArrayList<Video> already_have = new ArrayList<Video>();
+				for(Video item: temp)
+				{
+					if(item.getResolution().equals(max_resolution_value))
+					{
+						continue;
+					}
+					else
+					{
+						already_have.add(item);
+						temp.remove(item);
+					}
+				}
+				// TODO: thelo na dhmioyrgei thn lista meta missing videos apo to video me thn megalyterh resolution
+				missingVideos.addAll(video.getMissingVideos(format,resolution,max_resolution_loc));
+				// TODO: thelo na diagraftoyn ta already have apo thn lista missingVideos 
+				missingVideos.removeAll(already_have);
+				// TODO: thelo na diagraftoyn ta already have apo thn lista available gia na glitoso thn epanalhpsh
+				availableVideos.removeAll(already_have);
+			}
 		}
 	}
 }
--- Multimedia/src/Assignment/Video.java
+++ Multimedia/src/Assignment/Video.java
@@ -1,8 +1,88 @@
 package Assignment;
 
+import java.util.ArrayList;
+
 public class Video 
 {
-	String name;
-	String format;
-	String resolution;
+	private String name;
+	private String resolution;
+	private String format;
+	
+
+	public Video(String name,String resolution,String format) 
+	{
+		this.name = name;
+		this.resolution = resolution;
+		this.format = format;
+	}
+	
+	public ArrayList<Video> getMissingVideos (String[] format_list, String[] resolution_list,int resolution_loc)
+	{
+		ArrayList<Video> missingVideos = new ArrayList<Video>();
+		for (int j=0; j<format_list.length; j++)
+		{
+			for (int i=resolution_loc; i<resolution_list.length; i++)
+			{
+				if(format_list[j].equals(format) && resolution_list[i].equals(resolution))
+				{
+					continue;
+				}
+				else
+				{
+					Video video = new Video(name,format_list[j],resolution_list[i]);
+					missingVideos.add(video);
+				}
+			}
+		}
+		return missingVideos;
+	}
+	
+	public ArrayList<Video> searchByName ( ArrayList<Video> listofvideos)
+	{
+		ArrayList<Video> items_found = new ArrayList<Video>();
+		for(Video video: listofvideos)
+		{
+			if(this.name.equals(video.name))
+			{
+				items_found.add(video);
+			}
+		}
+		return items_found;
+	}
+	
+	public ArrayList<Video> deleteVideo (ArrayList<Video> list)
+	{
+		for (Video video: list)
+		{
+			if( this.name.equals(video.name) && this.format.equals(video.format) && this.resolution.equals(video.resolution))
+			{
+				list.remove(video);
+			}
+		}
+		return list;
+	}
+	
+	public boolean hasBetterResolution (String other_resolution)
+	{
+		String object_substring = resolution.substring(0,resolution.length()-1);
+		Integer  object_num_resolution = Integer.parseInt(object_substring);
+		String item_substring = other_resolution.substring(0,other_resolution.length()-1);
+		Integer  item_num_resolution = Integer.parseInt(item_substring);
+		if ( object_num_resolution >= item_num_resolution)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	
+	public void showVideoDetails ()
+	{
+		System.out.println(name + " " + resolution + " " + format);
+	}
+
+	public String getResolution() 
+	{
+		return resolution;
+	}
 }
